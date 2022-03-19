0bb13e9377db630949adab3eb5fc3190a3572bd9
viveknanaware <viveknanaware95@gmail.com>
2022-03-19 14:35:41 +0530

    Ravi chavan Production web

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Contact', component: ContactComponent },
+  { path: 'Home', component: HomeComponent },
+  { path: 'About', component: AboutusComponent },
+  { path: '', component: HomeComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
